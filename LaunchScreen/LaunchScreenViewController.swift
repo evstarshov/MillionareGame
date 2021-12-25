b30dd04f1383c634f572d39a98e52ee496f2151e
EugeneStarshov <yeenzo@yandex.ru>
2021-12-25 16:25:01 +0300

    new funcs: isvalid, showalert start game

--- LaunchScreen/LaunchScreenViewController.swift
+++ LaunchScreen/LaunchScreenViewController.swift
@@ -8,7 +8,11 @@
 import UIKit
 
 class LaunchScreenViewController: UIViewController {
-
+    
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var newGameButton: UIButton!
+    @IBOutlet weak var resultsButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -29,5 +33,39 @@ class LaunchScreenViewController: UIViewController {
        // Don't forget to reset when view is being removed
        AppUtility.lockOrientation(.all)
    }
+    
+    @IBAction func newGame(_ sender: Any) {
+        if isValid() == false {
+            print("game started")
+        } else {
+            showAlert()
+        }
+    }
+    
+    private func showAlert() {
+            let alertController = UIAlertController(
+                title: "Ошибка",
+                message: "Имя не может быть пустым",
+                preferredStyle: .alert)
+            let alertItem = UIAlertAction(
+                title: "Ok:(",
+                style: .cancel)
+            { _ in
+                self.nameTextField.text = ""
+            }
+            alertController.addAction(alertItem)
+            present(alertController,
+                    animated: true,
+                    completion: nil)
+        }
+    
+    private func isValid() -> Bool {
+        if nameTextField.text == ""
+             {
+            return true
+        } else {
+            return false
+        }
+    }
 
 }
