50842174383f5c13aed066ae2c65db01b10c400a
EugeneStarshov <yeenzo@yandex.ru>
2021-12-25 22:33:54 +0300

    func makeQustion1

--- GameScreen/GameViewController.swift
+++ GameScreen/GameViewController.swift
@@ -9,11 +9,31 @@ import UIKit
 
 class GameViewController: UIViewController {
 
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var answer1Button: UIButton!
+    @IBOutlet weak var answer2Button: UIButton!
+    @IBOutlet weak var answer3Button: UIButton!
+    @IBOutlet weak var answer4Button: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        let question = Question()
+        makeQuestion1(model: question)
     }
     
-    
+    func makeQuestion1(model: Question) {
+        let answers = model.questionDict.values.first
+        questionLabel.text = model.questionDict.keys.first
+        questionLabel.lineBreakMode = .byCharWrapping
+        let answer1 = answers![0]
+        let answer2 = answers![1]
+        let answer3 = answers![2]
+        let answer4 = answers![3]
+        answer1Button.setTitle(answer1, for: .normal)
+        answer2Button.setTitle(answer2, for: .normal)
+        answer3Button.setTitle(answer3, for: .normal)
+        answer4Button.setTitle(answer4, for: .normal)
+    }
 
 }
