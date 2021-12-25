becd75768aa650b276935fc4e28655c4522ec100
EugeneStarshov <yeenzo@yandex.ru>
2021-12-25 15:26:56 +0300

    Initial Commit

---
+++ MillionareGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MillionareGame
+//
+//  Created by Евгений Старшов on 25.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
