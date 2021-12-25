37ba6103d9d8990f23d626b42cbb409882834293
EugeneStarshov <yeenzo@yandex.ru>
2021-12-25 15:37:10 +0300

    Launch Screen

---
+++ LaunchScreen/LaunchScreenViewController.swift
@@ -0,0 +1,29 @@
+//
+//  LaunchScreenViewController.swift
+//  MillionareGame
+//
+//  Created by Евгений Старшов on 25.12.2021.
+//
+
+import UIKit
+
+class LaunchScreenViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
