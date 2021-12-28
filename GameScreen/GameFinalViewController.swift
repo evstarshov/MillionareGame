1c4dbc552be6d9a7942086ea5c28db3174c58e25
EugeneStarshov <yeenzo@yandex.ru>
2021-12-28 15:52:11 +0300

    Game Final ViewController

---
+++ GameScreen/GameFinalViewController.swift
@@ -0,0 +1,18 @@
+//
+//  GameFinalViewController.swift
+//  MillionareGame
+//
+//  Created by Евгений Старшов on 28.12.2021.
+//
+
+import UIKit
+
+class GameFinalViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+}
