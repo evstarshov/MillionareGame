ff12a5f8d1f13ef292402c2920e16b6ad67695b2
EugeneStarshov <yeenzo@yandex.ru>
2021-12-28 15:17:36 +0300

    return to launch screen button

--- GameScreen/GameOverViewController.swift
+++ GameScreen/GameOverViewController.swift
@@ -8,13 +8,20 @@
 import UIKit
 
 class GameOverViewController: UIViewController {
+    
+    @IBOutlet weak var returnToBeginBTN: NSLayoutConstraint!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
     }
     
-
+    @IBAction func toLaunchScreen() {
+        print("Returning to launch screen")
+        let vc = self.storyboard?.instantiateViewController(withIdentifier: "LaunchScreen") as! LaunchScreenViewController
+        self.present(vc, animated: true)
+    }
 
 
 }
