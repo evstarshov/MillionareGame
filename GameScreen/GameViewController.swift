6be1b079f0d3e8c8351f5ca82965044a825effeb
EugeneStarshov <yeenzo@yandex.ru>
2021-12-28 13:18:12 +0300

    struct Question refactoring

--- GameScreen/GameViewController.swift
+++ GameScreen/GameViewController.swift
@@ -15,25 +15,27 @@ class GameViewController: UIViewController {
     @IBOutlet weak var answer3Button: UIButton!
     @IBOutlet weak var answer4Button: UIButton!
     
+    var questions = [Question]()
+    var questionNumber = Int()
+    var answerNumber = Int()
+    var answeredQuestions: Int = 0
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        let question = Question()
-        makeQuestion1(model: question)
-    }
-    
-    func makeQuestion1(model: Question) {
-        let answers = model.questionDict.values.first
-        questionLabel.text = model.questionDict.keys.first
-        questionLabel.lineBreakMode = .byCharWrapping
-        let answer1 = answers![0]
-        let answer2 = answers![1]
-        let answer3 = answers![2]
-        let answer4 = answers![3]
-        answer1Button.setTitle(answer1, for: .normal)
-        answer2Button.setTitle(answer2, for: .normal)
-        answer3Button.setTitle(answer3, for: .normal)
-        answer4Button.setTitle(answer4, for: .normal)
+        questions = [
+        Question(question: "Из какого фильма Прекрасный принц?", answers: ["Золушка","Русалочка","Спящая Красавица","Мулан"], correctAnswer: 1),
+        Question(question: "Из какой страны родом Джастин Бибер?", answers: ["Канада","США","Франция","Англия"], correctAnswer: 1),
+        Question(question: "В сиквеле какого праздничного фильма снялся Дональд Трамп?", answers: ["Один Дома","Один дома 2","Ричи Рич","Маленькие негодяи"], correctAnswer: 2),
+        Question(question: "Какой герой мультфильма живет в ананасе под водой?", answers: ["Камбала","Немо","Рик и Морти","Губка Боб"], correctAnswer: 4),
+        Question(question: "Что является национальным животным Шотландии?", answers: ["Лошадь","Единорог","Волк","Корова"], correctAnswer: 2),
+        Question(question: "В каком известном романе фигурировали Джо, Мег, Бет и Эми Марч?", answers: ["Убить пересмешника","Том Сойер","Маленькие женщины","Моби Дик"], correctAnswer: 3)
+        ]
     }
 
+    
+    private func pickQuestion(model: Question) {
+        questionLabel.text = questions.randomElement()?.question
+    }
+    
 }
