01468b62234771b9c6e416f70f0b0a0146eab029
EugeneStarshov <yeenzo@yandex.ru>
2022-01-06 16:33:03 +0300

    addQuestionVC

---
+++ AddQuestion/AddQuestionViewController.swift
@@ -0,0 +1,27 @@
+//
+//  AddQuestionViewController.swift
+//  MillionareGame
+//
+//  Created by Евгений Старшов on 06.01.2022.
+//
+
+import UIKit
+
+class AddQuestionViewController: UIViewController {
+
+    @IBOutlet weak var questionTextBox: UITextField!
+    @IBOutlet weak var answer1TextBox: UITextField!
+    @IBOutlet weak var answer2TextBox: UITextField!
+    @IBOutlet weak var answer3TextBox: UITextField!
+    @IBOutlet weak var answer4TextBox: UITextField!
+    @IBOutlet weak var correctAnswerTextBox: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+
+}
